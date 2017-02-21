7e540fffbe0296e6e29e0f7ed0b20ced563ebe62
Leo Jacobs <leo@the-earthworks.com>
2017-02-21 14:26:31 +0000

    initial commit

---
+++ EarthworksWebsite/EarthworksWebsite/App/js/modules/includeTemplate.js
@@ -0,0 +1,51 @@
+﻿//Include Template
+(function () {
+    'use strict';
+    var s,
+        TmplObj = {
+            settings: {
+
+            },
+
+            init: function () {
+                s = this.settings;
+                this.bindUIActions();
+            },
+
+            bindUIActions: function () {
+                //Toggle Active Class on the link
+                $('#nav ul li a').on('click', function () {
+                    TmplObj.activeSection($(this));
+                });
+
+
+                //Partials
+                $('#overview').on('click', function () {
+                    TmplObj.tpl('_overview.html');
+                });
+
+                $('#forms').on('click', function () {
+                    TmplObj.tpl('_forms.html');
+                });
+
+
+
+                
+
+            },
+
+            activeSection: function ($clicked) {
+                $('#nav ul li a').removeClass('active');
+                $clicked.addClass('active');
+            },
+
+            tpl: function (url) {
+                $.get(url, function (data) {
+                    $('#content').html(data);
+                });
+            }
+        };
+
+    TmplObj.init();
+
+}());
---
+++ EarthworksWebsite/EarthworksWebsite/App/js/modules/partialTemplate.js
@@ -0,0 +1,23 @@
+﻿var partialTplSettings,
+    PartialTpl = {
+        settings : {
+            
+        },
+
+        init: function (el, url) {
+            partialTplSettings = this.settings;
+            partialTplSettings.$url = url;
+            partialTplSettings.$container = el;
+            this.PartTpl();
+        },
+
+        bindUIActions: function() {
+            
+        },
+
+        PartTpl: function() {
+            $.get(partialTplSettings.$url, function(data) {
+                $(partialTplSettings.$container).append(data);
+            });
+        }
+    };
